779d37dd021e9ccc03cd00b735c522ff75050457
Neal Sharma <sharman@sandiego.edu>
2014-09-25 11:08:00 -0700

    added my hello message

--- checkin.c
+++ checkin.c
@@ -2,9 +2,9 @@
 
 int main() {
 	printf("hello Quentin");
-	printf("Hello from Danny");	
+	printf("Hello from  Danny");
+	printf("Hello, it's Neal.");	
 	// add a printf to print out a welcome message that includes your name
 	printf("hello");	
-
 	return 0;
 }
