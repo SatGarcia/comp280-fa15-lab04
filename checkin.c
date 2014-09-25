902b95b006206d62f7e363c69e6a448775c0a226
haydenclevenger <haydenclevenger@sandiego.edu>
2014-09-25 11:15:21 -0700

    adding my hello message

--- checkin.c
+++ checkin.c
@@ -3,7 +3,5 @@
 int main() {
 	printf("COMP280 Roll Call:\n");
 	printf("hello world. It's me, Hayden.\n");	
-	// add a printf to print out a welcome message that includes your name
-
 	return 0;
 }
