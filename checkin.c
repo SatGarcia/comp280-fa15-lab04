3ad8dcdfe3f298bed871dbcc33f844e2db3970b7
Thomas Berry <tberry@sandiego.edu>
2014-09-30 10:58:15 -0700

    oh my god this is annoying

--- checkin.c
+++ checkin.c
@@ -25,7 +25,6 @@ int main() {
 	printf("20. Hi, it's Thomas");
 	// Add your hello message below here.
 	// Please give it a number (like above).
-	// Don't forget to end it with a newline ('\n')
-	printf("19. Hi, it's Thomas");	
+	// Don't forget to end it with a newline ('\n')	
 	return 0;
 }
