e470739bdcf02fe22d8f4d40e767a93663a9a22a
Sat Garcia <sat@sandiego.edu>
2014-09-30 09:34:23 -0700

    cleaned up existing greetings

--- checkin.c
+++ checkin.c
@@ -3,22 +3,26 @@
 int main() {
 	
 	printf("COMP280 Roll Call:\n");	
-	printf("1. Hello from Erin");
-	printf("2. hello");	
-	printf("3. hello Quentin");
-	printf("4. Hello, it's Neal.");	
-	printf("5. Hello from Danny");	
-	printf("6. Hello Brittany");
+	printf("1. Hello from Erin\n");
+	printf("2. hello\n");	
+	printf("3. hello Quentin\n");
+	printf("4. Hello, it's Neal.\n");	
+	printf("5. Hello from Danny\n");	
+	printf("6. Hello Brittany\n");
 	printf("7. Tanner Franklin\n");
 	printf("8. hello world. It's me, Hayden.\n");	
-	printf("9. Oh baby, hello there");
-	printf("10. Hello from Chris");
-	printf("11. Ryan says hello.");
-	printf("12. Charles WiNiN says hello.");
-	printf("13. Hello, Dion Amini");
-	printf("14. Hey, Jack");
-	printf("15. Hello, Setareh");
-	printf("16. Hello, Quentin");
-        printf("17. Hello, Erik\n");
+	printf("9. Oh baby, hello there\n");
+	printf("10. Hello from Chris\n");
+	printf("11. Ryan says hello.\n");
+	printf("12. Charles WiNiN says hello.\n");
+	printf("13. Hello, Dion Amini\n");
+	printf("14. Hey, Jack\n");
+	printf("15. Hello, Setareh\n");
+	printf("16. Hello, Quentin\n");
+	printf("17. Hello, Erik\n");
+
+	// Add your hello message below here.
+	// Don't forget to end it with a newline ('\n')
+	
 	return 0;
 }
