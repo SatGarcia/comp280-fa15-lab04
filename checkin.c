06f3c877e2d75edb2bfdc6cd681603a55d405b28
Quentin <fulsherqf@sandiego.edu>
2015-09-29 10:59:54 -0700

    added hello message to .c file

--- checkin.c
+++ checkin.c
@@ -4,6 +4,7 @@ int main() {
 
 	printf("COMP280 Roll Call:\n");
 	printf("1. Hello from Dr. Sat!\n");
+    printf("Hello from Quentin\n");
 
 	// Add your hello message below here.                                   
 	// Please give it a number (like above).                                
