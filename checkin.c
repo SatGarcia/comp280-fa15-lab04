93639e558d394f53be318dfae3b0005d45c8cf7c
Sat Garcia <sat@sandiego.edu>
2015-09-29 10:28:55 -0700

    reset for fa15

--- checkin.c
+++ checkin.c
@@ -1,52 +1,13 @@
 #include <stdio.h>
 
 int main() {
-	
-  // Add your hello message below here.                                   
-  // Please give it a number (like above).                                
-  // Don't forget to end it with a newline ('\n') 
+
 	printf("COMP280 Roll Call:\n");
-	printf("1. Hello from Erin\n");
-	printf("2. hello\n");	
-	printf("3. hello Quentin\n");
-	printf("4. Hello, it's Neal.\n");	
-	printf("5. Hello from Danny\n");	
-	printf("6. Hello Brittany\n");
-	printf("7. Tanner Franklin\n");
-	printf("8. hello world. It's me, Hayden.\n");	
-	printf("9. Oh baby, hello there\n");
-	printf("10. Hello from Chris\n");
-	printf("11. Ryan says hello.\n");
-	printf("12. Charles WiNiN says hello.\n");
-	printf("13. Hello, Dion Amini\n");
-	printf("14. Hey, Jack\n");
-	printf("15. Hello, Setareh\n");
-	printf("16. Hello, Quentin\n");
-	printf("17. Hello, Erik\n");
-	printf("18. Hola, from Satty\n");
-	printf("19. Konnichiwa, Gunnar desu\n");
-	printf("20. Hi, it's Thomas\n\n");
-	printf("21. ayyyy it's austin barry laliberte\n\n\n");	
-	printf("22. Hello this is Jeremy\n");
-	printf("23. Hi! From Bonnie!\n");
-	printf("24. Hi, it's Thomas\n");
-	printf("25. Hello from Casey\n");
-	printf("26. Hello, N\n");	
-	printf("27. Hello, Jasper Forest\n");
-	printf("28. Hello this is Jeremy\n");
-	printf("29. Hi! From Bonnie!\n");
-	printf("30. Hey, from Michael Levack!! \n");
-	printf("31. Hello, Noah. \n");
-	printf("32. Hello, it's Moath.\n");
-        printf("33. Hallo, ich bin Sean\n");
-	printf("34. Hello, from mingyu\n");
-	printf("35. Hello, Annan\n");
-        printf("36. Yo its big bog jay greezy on the track\n");
-       	printf("37. Yo its ya boi Kyle aka swagamillion\n");
-	printf("38. Hello, it's Heath\n");
-	printf("39. Konnbanwa, TJ da.\n");
-	printf("40. Hello, Mark\n");
-	printf ("41. Hello, it's Molly\n");
-	printf ("42. Hello, DONNA\n");
+	printf("1. Hello from Dr. Sat!\n");
+
+	// Add your hello message below here.                                   
+	// Please give it a number (like above).                                
+	// Don't forget to end it with a newline ('\n') 
+	
 	return 0;
 }
